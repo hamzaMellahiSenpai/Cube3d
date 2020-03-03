2905f85a711e0d67a623578308c1b295a696f158
Hamza Mellahi <hmellahi@e2r11p12.1337.ma>
2020-03-03 09:06:28 +0100

    note

--- objects/sprite.c
+++ objects/sprite.c
@@ -6,7 +6,7 @@
 /*   By: hmellahi <hmellahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 08:29:20 by hmellahi          #+#    #+#             */
-/*   Updated: 2020/03/03 08:35:01 by hmellahi         ###   ########.fr       */
+/*   Updated: 2020/03/03 09:06:21 by hmellahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,18 @@ void    sp()
 {
      
 }
+
+// Angle Btw sprite and Player
+Angle = atan2(spriteY - PY, spriteX, PX);
+while (angle - playerRotation) > 2 * 
+P 
+	angle -= 2P
+While (angle - playerRotation) < P
+	angle += 2P
+GET POS OF SPRITE AND HEIGHT
+Y        = Y * w / (60 TO RAD)
+X       = 
+SIZE = WIDTH / 
+
+https://github.com/3xme/cub3D
+https://www.allegro.cc/forums/thread/355015
